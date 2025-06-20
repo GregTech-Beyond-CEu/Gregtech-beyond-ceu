693be5376e406e1fb55d687538873f436bcb8016
BLocky104 <farmermick13@web.de>
2025-06-20 18:21:01 +0200

    remove old Silicone Rubber Processing

---
+++ groovy/postInit/Chemistry/Organic Chemistry/Polymers/Silicone Rubber.groovy
@@ -0,0 +1,6 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+
+// Liquid Silicone Rubber * 1296
+mods.gregtech.large_chemical_reactor.removeByInput(30, [metaitem('dustPolydimethylsiloxane') * 9, metaitem('dustSulfur')], null)
