af6709602712735a1af22737568c50e5d64899bd
BLocky104 <farmermick13@web.de>
2025-07-14 16:11:59 +0200

    Create Hydrogen Cyanide.groovy

---
+++ groovy/postInit/Chemistry/Inorganic Chemistry/Hydrogen Cyanide.groovy
@@ -0,0 +1,15 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+//Andrussow Process
+
+    CHEMICAL_RECIPES.recipeBuilder()
+        .fluidInputs(fluid('methane')*1000)
+        .fluidInputs(fluid('ammonia')*1000)
+        .fluidInputs(fluid('oxygen')*3000)
+        .fluidOutputs(fluid('water')*3000)
+        .notConsumable(metaitem('dustPlatinum'))
+        .fluidOutputs(fluid('gtfo_hydrogen_cyanide') * 1000)
+        .EUt(40)
+        .duration(180)
+        .buildAndRegister();
