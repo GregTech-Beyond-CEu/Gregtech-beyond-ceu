a5a082b314bf15aa34d351a07fcf07720be18b49
BLocky104 <farmermick13@web.de>
2025-07-14 16:09:13 +0200

    BAP Compat

---
+++ groovy/postInit/Mods/BetterAnimalsPlus.groovy
@@ -0,0 +1,29 @@
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gtb.api.recipes.GTBRecipeMaps.*;
+
+crafting.remove('betteranimalsplus:wolf_pelt_black_leather')
+crafting.remove('betteranimalsplus:wolf_pelt_timber_leather')
+crafting.remove('betteranimalsplus:wolf_pelt_snowy_leather')
+crafting.remove('betteranimalsplus:wolf_pelt_red_leather')
+crafting.remove('betteranimalsplus:wolf_pelt_brown_leather')
+crafting.remove('betteranimalsplus:wolf_pelt_arctic_leather')
+
+
+crafting.remove('betteranimalsplus:bear_skin_brown_leather')
+crafting.remove('betteranimalsplus:bear_skin_kermode_leather')
+crafting.remove('betteranimalsplus:bear_skin_black_leather')
+
+
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_black'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_timber'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_snowy'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_red'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_brown'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_arctic'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_black'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:wolf_pelt_black'))
+
+
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:bear_skin_brown'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:bear_skin_kermode'))
+mods.minecraft.ore_dict.add('hideScrapeable', item('betteranimalsplus:bear_skin_black'))
