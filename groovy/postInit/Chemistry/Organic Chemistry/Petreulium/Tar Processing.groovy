60365dd3525038df352937512ff64f276427c1b9
BLocky104 <farmermick13@web.de>
2025-07-14 16:11:13 +0200

    Update Tar Processing.groovy

--- groovy/postInit/Chemistry/Organic Chemistry/Petreulium/Tar Processing.groovy
+++ groovy/postInit/Chemistry/Organic Chemistry/Petreulium/Tar Processing.groovy
@@ -3,6 +3,4 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
 
 
 // Creosote * 300
-mods.gregtech.distillation_tower.removeByInput(256, null, [fluid('wood_tar') * 1000 * 1000])
-// Phenol * 100
-mods.gregtech.distillation_tower.removeByInput(780, null, [fluid('coal_tar') * 1000 * 1000])
+mods.gregtech.distillation_tower.removeByInput(256, null, [fluid('wood_tar') * 1000])
