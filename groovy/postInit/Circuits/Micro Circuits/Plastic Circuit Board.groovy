2eca17088a4e3b21bd87afff5fb6a7fe570b6d7f
BLocky104 <farmermick13@web.de>
2025-07-14 16:10:33 +0200

    Update Plastic Circuit Board.groovy

--- groovy/postInit/Circuits/Micro Circuits/Plastic Circuit Board.groovy
+++ groovy/postInit/Circuits/Micro Circuits/Plastic Circuit Board.groovy
@@ -36,21 +36,28 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
                 .buildAndRegister();
 
         CHEMICAL_BATH_RECIPES.recipeBuilder()
-                .fluidInputs(fluid('iron_iii_chloride') * 100)
-                .inputs(metaitem('board.plastic'))
-                .outputs(metaitem('circuit_board.plastic'))
+                .fluidInputs(fluid('sodium_bisulfate_solution')*100)
+                .inputs(metaitem('patterned_plastic_board'))
+                .outputs(metaitem('drenched_plastic_board'))
                 .duration(200)
                 .EUt(80)
                 .buildAndRegister();
 
         CHEMICAL_BATH_RECIPES.recipeBuilder()
-                .fluidInputs(fluid('sodium_bisulfate_solution')*100)
-                .inputs(metaitem('board.plastic'))
-                .outputs(metaitem('circuit_board.plastic'))
+                .fluidInputs(fluid('iron_iii_chloride')*100)
+                .inputs(metaitem('patterned_plastic_board'))
+                .outputs(metaitem('drenched_plastic_board'))
                 .duration(200)
                 .EUt(80)
                 .buildAndRegister();
 
+        ENGRAVING_UNIT_RECIPES.recipeBuilder()
+                .fluidInputs(fluid('novolacs_photoresist')*200)
+                .inputs(metaitem('board.plastic'))
+                .outputs(metaitem('patterned_plastic_board'))
+                .duration(200)
+                .EUt(80)
+                .buildAndRegister();
         
         MIXER_RECIPES.recipeBuilder()
                 .fluidInputs(fluid('water')*1000)
@@ -68,3 +75,32 @@ import static gtb.api.recipes.GTBRecipeMaps.*;
                 .EUt(90)
                 .buildAndRegister();
 
+
+// Plastic Circuit Board * 1
+mods.gregtech.chemical_reactor.removeByInput(10, [metaitem('platePlastic'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 2
+mods.gregtech.chemical_reactor.removeByInput(10, [metaitem('platePolyvinylChloride'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 4
+mods.gregtech.chemical_reactor.removeByInput(10, [metaitem('platePolytetrafluoroethylene'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 8
+mods.gregtech.chemical_reactor.removeByInput(10, [metaitem('platePolybenzimidazole'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 1
+mods.gregtech.large_chemical_reactor.removeByInput(30, [metaitem('board.plastic'), metaitem('foilCopper') * 6], [fluid('sodium_persulfate') * 500 * 500])
+// Plastic Circuit Board * 1
+mods.gregtech.large_chemical_reactor.removeByInput(30, [metaitem('board.plastic'), metaitem('foilCopper') * 6], [fluid('iron_iii_chloride') * 250 * 250])
+// Plastic Circuit Board * 1
+mods.gregtech.large_chemical_reactor.removeByInput(10, [metaitem('platePlastic'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 2
+mods.gregtech.large_chemical_reactor.removeByInput(10, [metaitem('platePolyvinylChloride'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 4
+mods.gregtech.large_chemical_reactor.removeByInput(10, [metaitem('platePolytetrafluoroethylene'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+// Plastic Circuit Board * 8
+mods.gregtech.large_chemical_reactor.removeByInput(10, [metaitem('platePolybenzimidazole'), metaitem('foilCopper') * 4], [fluid('sulfuric_acid') * 250 * 250])
+
+        FORMING_PRESS_RECIPES.recipeBuilder()
+                .inputs(metaitem('drenched_plastic_board'))
+                .inputs(metaitem('foilGold')*2)
+                .outputs(metaitem('circuit_board.plastic'))
+                .duration(120)
+                .EUt(90)
+                .buildAndRegister();
