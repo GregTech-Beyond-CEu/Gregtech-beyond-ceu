428638cf8dd9b4bcf9c26607dd560e1c91dd01be
BLocky104 <farmermick13@web.de>
2025-08-04 19:50:29 +0200

    Update Polyvinyl Butyral.groovy

--- groovy/postInit/Chemistry/Organic Chemistry/Polymers/Polyvinyl Butyral.groovy
+++ groovy/postInit/Chemistry/Organic Chemistry/Polymers/Polyvinyl Butyral.groovy
@@ -15,3 +15,5 @@ POLYMERIZATION_TANK_RECIPES.recipeBuilder()
 mods.gregtech.chemical_reactor.removeByInput(480, null, [fluid('butyraldehyde') * 250 * 250, fluid('polyvinyl_acetate') * 144 * 144])
 // Liquid Polyvinyl Butyral * 144
 mods.gregtech.large_chemical_reactor.removeByInput(480, null, [fluid('butyraldehyde') * 250 * 250, fluid('polyvinyl_acetate') * 144 * 144])
+// Butyraldehyde * 1000
+mods.gregtech.large_chemical_reactor.removeByInput(480, null, [fluid('propene') * 1000 * 1000, fluid('hydrogen') * 2000 * 2000, fluid('carbon_monoxide') * 1000 * 1000])
