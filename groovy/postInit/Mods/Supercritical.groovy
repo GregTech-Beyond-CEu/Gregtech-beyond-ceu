a5791130219fa257673e2ee691791806f49cae6c
BLocky104 <farmermick13@web.de>
2025-08-04 19:49:57 +0200

    Update Supercritical.groovy

--- groovy/postInit/Mods/Supercritical.groovy
+++ groovy/postInit/Mods/Supercritical.groovy
@@ -11,14 +11,10 @@ mods.gregtech.heat_exchanger.removeByInput(1, [metaitem('circuit.integrated').wi
 // Heavy Water * 1
 mods.gregtech.heat_exchanger.removeByInput(1, [metaitem('circuit.integrated').withNbt(['Configuration': 1])], [fluid('high_pressure_heavy_water'), fluid('distilled_water') * 6 * 6])
 
-// Reactor Vessel * 2
-mods.gregtech.assembler.removeByInput(48, [metaitem('supercritical:plateDoubleInconel'), metaitem('plateSteel') * 5, metaitem('frameSteel')], null)
-
-
 ASSEMBLER_RECIPES.recipeBuilder()
     .inputs(metaitem('plateHighPurityStainlessSteel')*5)
     .inputs(item('gregtech:meta_block_frame_917', 8))
-    .inputs(metaitem('supercritical:plateDoubleInconel'))
+    .inputs(metaitem('plateDoubleInconel718'))
     .outputs(item('supercritical:fission_casing')*2)
     .EUt(180)
     .duration(500)
